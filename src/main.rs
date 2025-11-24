f0efc07ca89a496d3f2cf48e8d80651dd11c210a
aditya yadav <dedomil@tuta.io>
2025-11-25 02:58:39 +0530

    add clap crate and create main command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use clap::{Arg, Command};
+
 mod instagram;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = Command::new("compressta")
+        .about("Share your Instagram photos as CNFT's")
+        .subcommand(
+            Command::new("create_tree")
+                .about("Create a new Bubblegum Tree")
+                .arg(
+                    Arg::new("Max depth")
+                        .short('d')
+                        .long("max-depth")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("Max buffer size")
+                        .short('b')
+                        .long("max-buffer-size")
+                        .required(true),
+                )
+                .arg(Arg::new("Public").short('p').long("public").required(false)),
+        )
+        .get_matches();
+
     Ok(())
 }
